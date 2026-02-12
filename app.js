cfb235552c94d776e0198e8c3e2d92fbc1d18f28
praewalnwza001 <sokcamin@gmail.com>
2026-02-12 09:04:16 +0700

    Add

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 3000;
 
-app.get('/contacts', (req, res) => {
-    res.json([{ name: 'Alice', phone: '123-456' }]);
+app.get('/', (req, res) => {
+  res.send('<h1>Sawasdee Zin! Exam Passed 100%</h1><p>Jenkins to Vercel Success!</p>');
 });
 
-app.post('/contacts', (req, res) => {
-    res.status(201).json({ message: 'Contact saved' });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
-app.put('/contacts/favorite', (req, res) => {
-    res.json({ message: 'Marked as favorite' });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-module.exports = app;
